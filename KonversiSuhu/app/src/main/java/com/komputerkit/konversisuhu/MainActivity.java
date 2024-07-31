e28a18d0f403fb0336fcdcf60d4dd7376a00e9dc
im nanaa <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-07-31 12:51:48 +0700

    Update MainActivity.java

--- KonversiSuhu/app/src/main/java/com/komputerkit/konversisuhu/MainActivity.java
+++ KonversiSuhu/app/src/main/java/com/komputerkit/konversisuhu/MainActivity.java
@@ -16,6 +16,37 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        });
+        };
+    public void load(){
+        spinner=findViewById(R.id.spinner);
+        etNilai=findViewById(R.id.etNilai);
+        tvHasil=findViewById(R.id.tvHasil);
     }
-}
+
+//    public void isiSpinner(){
+//        String[] isi={"celsius to remaur", "to kelvin"};
+//        ArrayAdapter<String> adapter=new ArrayAdapter<String>(context: this,android.R.layout.simple_spinner_item,isi);
+//    }
+    public void btnKonversi(View view){
+     String pilihan=spinner.getSelectedItem().toString();
+
+     if(etNilai.getText().toString().equals("")){
+         Toast.makeText(context: this, text:"nilai tidak boleh kosong", Toast.LENGTH_SHORT).show();
+     } else{
+         if(pilihan.equals("Celcius to Reamur")){
+             cToR();
+         }  if(pilihan.equals("Celcius to Fahrenheit")){
+             cToF();
+         }
+     }
+     System.out.printl(pilihan);
+     cTor();}
+    public void cToR(){
+        double suhu=Double.parseDouble(etNilai.getText().toString());
+        double hasil=(4.0/5.0)*suhu;
+        tvHasil.seText(hasil+"");
+    }
+    public void cToF(){
+        Toast.makeText(context: this, text:"belum dibuat", Toast.LENGTH_SHORT).show();
+    }
+    }
