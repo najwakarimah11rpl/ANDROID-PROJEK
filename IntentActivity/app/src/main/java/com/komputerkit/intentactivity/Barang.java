266078328989d77421b11e9ea277952939cd0588
im nanaa <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-08-01 11:22:03 +0700

    Add files via upload

---
+++ IntentActivity/app/src/main/java/com/komputerkit/intentactivity/Barang.java
@@ -0,0 +1,26 @@
+package com.komputerkit.intentactivity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Barang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_barang);
+       load();
+       ambilData();
+    } public void load(){
+        tvbarang=findViewById(R.id.tvBarang);
+    } public void ambilData(){
+        String ambil=getIntent().getStringExtra(name= "ISI");
+        tvbarang.setText(ambil);
+    }
+}
