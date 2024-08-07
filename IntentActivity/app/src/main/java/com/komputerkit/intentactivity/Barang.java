b54b7b149b49a43f8fee2fc57052507f980b67d8
im nanaa <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-08-07 18:14:22 +0700

    Update Barang.java

--- IntentActivity/app/src/main/java/com/komputerkit/intentactivity/Barang.java
+++ IntentActivity/app/src/main/java/com/komputerkit/intentactivity/Barang.java
@@ -1,26 +1,30 @@
 package com.komputerkit.intentactivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Barang extends AppCompatActivity {
+
+    private TextView tvBarang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_barang);
-       load();
-       ambilData();
-    } public void load(){
-        tvbarang=findViewById(R.id.tvBarang);
-    } public void ambilData(){
-        String ambil=getIntent().getStringExtra("ISI");
-        tvbarang.setText(ambil);
+        load();
+        ambilData();
+    }
+
+    private void load() {
+        tvBarang = findViewById(R.id.tvBarang);
+    }
+
+    private void ambilData() {
+        String ambil = getIntent().getStringExtra("ISI");
+        if (ambil != null) {
+            tvBarang.setText(ambil);
+        }
     }
 }
