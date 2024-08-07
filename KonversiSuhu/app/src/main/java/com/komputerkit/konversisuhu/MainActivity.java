54f04f07c3590b82eacd30b4b18cf00162043ca4
im nanaa <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-08-07 18:05:12 +0700

    Update MainActivity.java

--- KonversiSuhu/app/src/main/java/com/komputerkit/konversisuhu/MainActivity.java
+++ KonversiSuhu/app/src/main/java/com/komputerkit/konversisuhu/MainActivity.java
@@ -1,14 +1,21 @@
 package com.komputerkit.konversisuhu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private EditText etNilai;
+    private TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,37 +23,55 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        };
-    public void load(){
-        spinner=findViewById(R.id.spinner);
-        etNilai=findViewById(R.id.etNilai);
-        tvHasil=findViewById(R.id.tvHasil);
+        load();
+        isiSpinner();
     }
 
-//    public void isiSpinner(){
-//        String[] isi={"celsius to remaur", "to kelvin"};
-//        ArrayAdapter<String> adapter=new ArrayAdapter<String>(context: this,android.R.layout.simple_spinner_item,isi);
-//    }
-    public void btnKonversi(View view){
-     String pilihan=spinner.getSelectedItem().toString();
+    private void load() {
+        spinner = findViewById(R.id.spinner);
+        etNilai = findViewById(R.id.etNilai);
+        tvHasil = findViewById(R.id.tvHasil);
+    }
 
-     if(etNilai.getText().toString().equals("")){
-         Toast.makeText(context: this, text:"nilai tidak boleh kosong", Toast.LENGTH_SHORT).show();
-     } else{
-         if(pilihan.equals("Celcius to Reamur")){
-             cToR();
-         }  if(pilihan.equals("Celcius to Fahrenheit")){
-             cToF();
-         }
-     }
-     System.out.printl(pilihan);
-     cTor();}
-    public void cToR(){
-        double suhu=Double.parseDouble(etNilai.getText().toString());
-        double hasil=(4.0/5.0)*suhu;
-        tvHasil.seText(hasil+"");
+    private void isiSpinner() {
+        String[] isi = {"Celsius to Reamur", "Celsius to Fahrenheit", "Celsius to Kelvin"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, isi);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
-    public void cToF(){
-        Toast.makeText(context: this, text:"belum dibuat", Toast.LENGTH_SHORT).show();
+
+    public void btnKonversi(View view) {
+        String pilihan = spinner.getSelectedItem().toString();
+
+        if (etNilai.getText().toString().equals("")) {
+            Toast.makeText(this, "Nilai tidak boleh kosong", Toast.LENGTH_SHORT).show();
+        } else {
+            if (pilihan.equals("Celsius to Reamur")) {
+                cToR();
+            } else if (pilihan.equals("Celsius to Fahrenheit")) {
+                cToF();
+            } else if (pilihan.equals("Celsius to Kelvin")) {
+                cToK();
+            }
+        }
+        System.out.println(pilihan);
     }
+
+    private void cToR() {
+        double suhu = Double.parseDouble(etNilai.getText().toString());
+        double hasil = (4.0 / 5.0) * suhu;
+        tvHasil.setText(hasil + "");
     }
+
+    private void cToF() {
+        double suhu = Double.parseDouble(etNilai.getText().toString());
+        double hasil = (9.0 / 5.0) * suhu + 32;
+        tvHasil.setText(hasil + "");
+    }
+
+    private void cToK() {
+        double suhu = Double.parseDouble(etNilai.getText().toString());
+        double hasil = suhu + 273.15;
+        tvHasil.setText(hasil + "");
+    }
+}
