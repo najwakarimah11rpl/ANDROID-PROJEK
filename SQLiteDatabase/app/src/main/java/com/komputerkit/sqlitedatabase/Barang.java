830adfb24c0d3a94d377a002be3c4559119e0ac5
im nanaa <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-08-07 18:33:23 +0700

    Update Barang.java

--- SQLiteDatabase/app/src/main/java/com/komputerkit/sqlitedatabase/Barang.java
+++ SQLiteDatabase/app/src/main/java/com/komputerkit/sqlitedatabase/Barang.java
@@ -1,77 +1,44 @@
 package com.komputerkit.sqlitedatabase;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.PopupMenu;
-import android.widget.TextView;
-import android.widget.Toast;
+public class Barang {
+    private String idbarang,barang,stok,harga;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.List;
-
-public class BarangAdapter extends RecyclerView.Adapter<BarangAdapter.ViewHolder> {
-    private Context context;
-    private List<Barang> barangList;
-
-    public BarangAdapter(Context context, List<Barang> barangList) {
-        this.context = context;
-        this.barangList = barangList;
+    public Barang(String idbarang, String barang, String stok, String harga) {
+        this.idbarang = idbarang;
+        this.barang = barang;
+        this.stok = stok;
+        this.harga = harga;
     }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.item_barang, parent, false);
-        return new ViewHolder(v);
+    public String getIdbarang() {
+        return idbarang;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Barang barang = barangList.get(position);
-        holder.tvBarang.setText(barang.getBarang());
-        holder.tvStok.setText(barang.getStok());
-        holder.tvHarga.setText(barang.getHarga());
-
-        holder.tvMenu.setOnClickListener(v -> {
-            PopupMenu popupMenu = new PopupMenu(context, holder.tvMenu);
-            popupMenu.inflate(R.menu.menu_item);
-
-            popupMenu.setOnMenuItemClickListener(item -> {
-                switch (item.getItemId()) {
-                    case R.id.ubah:
-                        ((MainActivity) context).selectUpdate(barang.getIdbarang());
-                        return true;
-                    case R.id.hapus:
-                        ((MainActivity) context).deleteData(barang.getIdbarang());
-                        return true;
-                    default:
-                        return false;
-                }
-            });
-
-            popupMenu.show();
-        });
+    public void setIdbarang(String idbarang) {
+        this.idbarang = idbarang;
     }
 
-    @Override
-    public int getItemCount() {
-        return barangList.size();
+    public String getBarang() {
+        return barang;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView tvBarang, tvStok, tvHarga, tvMenu;
+    public void setBarang(String barang) {
+        this.barang = barang;
+    }
 
-        public ViewHolder(View itemView) {
-            super(itemView);
-            tvBarang = itemView.findViewById(R.id.tvBarang);
-            tvStok = itemView.findViewById(R.id.tvStok);
-            tvHarga = itemView.findViewById(R.id.tvHarga);
-            tvMenu = itemView.findViewById(R.id.tvMenu);
-        }
+    public String getStok() {
+        return stok;
+    }
+
+    public void setStok(String stok) {
+        this.stok = stok;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
     }
 }
