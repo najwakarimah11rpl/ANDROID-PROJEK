e545554f72a0835a7b024c45bf73f9396206bc55
im nanaa <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-08-07 18:19:45 +0700

    Update MainActivity.java

--- MessageDialog/app/src/main/java/com/komputerkit/messagedialog/MainActivity.java
+++ MessageDialog/app/src/main/java/com/komputerkit/messagedialog/MainActivity.java
@@ -2,46 +2,58 @@ package com.komputerkit.messagedialog;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        // Initialize any UI components or set up listeners here if needed
+    }
 
-        public void showToast(String pesan){
-            Toast.makeText(context=this, pesan, Toast.LENGTH_SHORT).show();
-        }
-        public void showAlert(String pesan){
-            AlertDialog.Builder buatAlert=new AlertDialog.Builder(context= this);
-            buatAlert.setTitle("PERHATIAN!");
-            buatAlert.setMessage(pesan);
-            buatAlert.show();
-        }
-        public void showAlertButton(String pesan){
-            AlertDialog.Builder showAlert=new AlertDialog.Builder(context= this);
-            showAlert.setTitle("PERINGATAN");
-            showAlert.setMessage(pesan);
+    public void showToast(String pesan) {
+        Toast.makeText(this, pesan, Toast.LENGTH_SHORT).show();
+    }
 
-            showAlert.setPositiveButton(text="NO", new DialogInterface.OnCancelListener()){
-                public void onClick(DialogInterface dialog, int which){
-                    showToast(pesan= "data syudah dihapus");
-                }
-            } showAlert.show();
-        }
-        public void btnToast(View view){
-            showToast(pesan= "selamat belajar");
-        } public void btnAlert(View view){
-            showAlert(pesan= "selamat belajar");
-        } public void btnAlertDialogButton(View view){
-            showAlertButton(pesan= "yakin akan menghapus?");
-        }
-    }}
+    public void showAlert(String pesan) {
+        AlertDialog.Builder buatAlert = new AlertDialog.Builder(this);
+        buatAlert.setTitle("PERHATIAN!");
+        buatAlert.setMessage(pesan);
+        buatAlert.setPositiveButton("OK", null); // Optional positive button
+        buatAlert.show();
+    }
+
+    public void showAlertButton(String pesan) {
+        AlertDialog.Builder showAlert = new AlertDialog.Builder(this);
+        showAlert.setTitle("PERINGATAN");
+        showAlert.setMessage(pesan);
+
+        showAlert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                showToast("Data sudah dihapus");
+            }
+        });
+
+        showAlert.setNegativeButton("NO", null);
+        showAlert.show();
+    }
+
+    public void btnToast(View view) {
+        showToast("Selamat belajar");
+    }
+
+    public void btnAlert(View view) {
+        showAlert("Selamat belajar");
+    }
+
+    public void btnAlertDialogButton(View view) {
+        showAlertButton("Yakin akan menghapus?");
+    }
+}
