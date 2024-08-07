06a51cde15b55fc98d8674c701628a61412f45ee
im nanaa <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-08-07 18:18:45 +0700

    Update MainActivity.java

--- Kalkulator/app/src/main/java/com/komputerkit/kalkulator/MainActivity.java
+++ Kalkulator/app/src/main/java/com/komputerkit/kalkulator/MainActivity.java
@@ -1,14 +1,18 @@
 package com.komputerkit.kalkulator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etBil_1, etBil_2;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,24 +20,23 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         load();
+    }
 
-        public void load() {
-        tvHasil= findViewById(R.id.tvHasil);}
-        etBil_1=findViewById(R.id.etBil_1);
-        etBil_2=findViewById(R.id.etBil_2);
+    private void load() {
+        tvHasil = findViewById(R.id.tvHasil);
+        etBil_1 = findViewById(R.id.etBil_1);
+        etBil_2 = findViewById(R.id.etBil_2);
+    }
 
-        public void btnJumlah(View view){
-            if (etBil_1.getText().toString().equals("") || etBil_2.getText().toString().equals("")){
-                Toast.makeText(context;this, text;"ada bilangan yang kosong",Toast.LENGHT_SHORT)
-                show();
+    public void btnJumlah(View view) {
+        if (etBil_1.getText().toString().isEmpty() || etBil_2.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Ada bilangan yang kosong", Toast.LENGTH_SHORT).show();
+        } else {
+            double bil_1 = Double.parseDouble(etBil_1.getText().toString());
+            double bil_2 = Double.parseDouble(etBil_2.getText().toString());
+
+            double hasil = bil_1 + bil_2;
+            tvHasil.setText(String.valueOf(hasil));
         }
-        else if{
-
-            double bil_1=Double.parseDouble(etBil_1.getText(). toString());
-            double bil_2=Double.parseDouble(etBil_2.getText(). toString());
-
-            double hasil=bil_1+bil_2;
-            tvHasil.seText(hasil+"");}
-        else if{
-            double hasil=bil_1-bil_2;
-            tvHasil.seText(hasil+"");}}}}
+    }
+}
