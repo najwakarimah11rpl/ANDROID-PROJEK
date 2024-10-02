92f0049d41fb2b7a3ed00e3d96361f67fce83074
nanaa <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-10-02 14:24:43 +0700

    Update MainActivity.java

--- Webview/app/src/main/java/com/nana/webview/MainActivity.java
+++ Webview/app/src/main/java/com/nana/webview/MainActivity.java
@@ -16,13 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myWebView = findViewById(R.id.webview);
-        myWebView.setWebViewClient(new WebViewClient()); // Untuk membuka link di WebView
+        myWebView.setWebViewClient(new WebViewClient()); 
         myWebView.loadUrl("https://alphatechin.id/#:~:text=Alphatech%20solusi%20permasalahan%20teknologi%20dan%20bisnis%20Anda%20dengan");
 
-        // Mengatur pengaturan WebView
+        
         WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true); // Mengaktifkan JavaScript
-        webSettings.setDomStorageEnabled(true); // Mengaktifkan penyimpanan DOM
+        webSettings.setJavaScriptEnabled(true); 
+        webSettings.setDomStorageEnabled(true); 
     }
 
     @Override
