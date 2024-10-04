5191fee199856ea53bd27da0d63bb6013e49f23c
nanaa <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-10-04 08:53:07 +0700

    Update MainActivity.java

--- Webview/app/src/main/java/com/nana/webview/MainActivity.java
+++ Webview/app/src/main/java/com/nana/webview/MainActivity.java
@@ -1,5 +1,7 @@
 package com.nana.webview;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -16,13 +18,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myWebView = findViewById(R.id.webview);
-        myWebView.setWebViewClient(new WebViewClient()); 
-        myWebView.loadUrl("https://alphatechin.id/#:~:text=Alphatech%20solusi%20permasalahan%20teknologi%20dan%20bisnis%20Anda%20dengan");
-
-        
         WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true); 
-        webSettings.setDomStorageEnabled(true); 
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        // Set the WebViewClient before loading the URL
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView wView, String url) {
+                if (url.startsWith("mailto:") || url.startsWith("tel:") || url.startsWith("geo:") || url.startsWith("http:") || url.startsWith("https:")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                } else if (url.startsWith("whatsapp:")) {
+                    Intent sendIntent = new Intent();
+                    sendIntent.setAction(Intent.ACTION_SEND);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+                    sendIntent.setType("text/plain");
+                    sendIntent.setPackage("com.whatsapp");
+                    startActivity(sendIntent);
+                    return true;
+                } else if (url.startsWith("spotify:")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("spotify:album:0sNOF9WDwhWunNAHPD3Baj"));
+                    intent.putExtra(Intent.EXTRA_REFERRER, Uri.parse("android-app://" + getPackageName()));
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // Load the initial URL
+        myWebView.loadUrl("https://alphatechin.id/#:~:text=Alphatech%20solusi%20permasalahan%20teknologi%20dan%20bisnis%20Anda%20dengan");
     }
 
     @Override
